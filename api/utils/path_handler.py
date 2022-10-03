a591cc63156900f37771ca8aa27b5e59cb9da137
Alex <automation.van@gmail.com>
2022-10-03 12:19:24 -0700

    update for jenkins relative dir.

--- api/utils/path_handler.py
+++ api/utils/path_handler.py
@@ -7,8 +7,8 @@ cases_dir = os.path.join(base_dir, "testCases")
 # Test data dir
 data_dir = os.path.join(base_dir, "testData")
 # Test report dir
-reports_dir = os.path.join(base_dir, "output\\reports")
+reports_dir = os.path.join(base_dir, "output", "reports")
 # Test logs dir
-logs_dir = os.path.join(base_dir, "output\\logs")
+logs_dir = os.path.join(base_dir, "output", "logs")
 # configuration dir
 conf_dir = os.path.join(base_dir, "config")
